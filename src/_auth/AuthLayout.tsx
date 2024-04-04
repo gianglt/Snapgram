d254a15c364be234c9b700f6afaae93ac91a15e1
Giang <gianglt@gmail.com>
2024-04-04 08:45:24 +0700

    Init Learning

---
+++ src/_auth/AuthLayout.tsx
@@ -0,0 +1,22 @@
+import { Outlet, Navigate } from "react-router-dom"
+
+const AuthLayout = () => {
+
+  const isAuthenticated = false;
+
+
+  return (
+    <>
+    {isAuthenticated ? (<Navigate to="/"/>)
+    : (
+      <>
+      <section className="flex flex-1 ">
+        <Outlet/>
+      </section>
+      </>
+    )}
+    </>
+  )
+}
+
+export default AuthLayout
